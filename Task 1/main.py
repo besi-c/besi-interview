15d2e1e8ce70eb1c6f7dacd149896b7f1ce52713
Fiji Marcelin <fiji4real@gmail.com>
2022-04-19 23:21:15 -0400

    restructured Task 1 to be up to spec

--- Task 1/main.py
+++ Task 1/main.py
@@ -2,17 +2,19 @@ from lib import Read, Analyze
 import time
 import numpy
 
+checkAnalyze = []
 frameStreamData = [] # stream "storage"
-def combineAndAnalyze(rawFrames,lastframe):
-    while((lastframe+1)<len(rawFrames)): #rawFrames is the frameStream data
-        frameOne= numpy.array(rawFrames[lastframe]) #get first raw frame
-        frameTwo = numpy.array(rawFrames[lastframe + 1]) #get second raw frame
-        combinedFrame= numpy.concatenate((frameOne,frameTwo),axis=1) #combine frames
-        # My understanding I am combining two numpy objects as one array, not a python array, so Analyze is being called
-        # on 1 numpy object with an array of length 20 since each small array is length 10
-        Analyze(combinedFrame) #call analyze on combined frames, Analyze should append combined numpy array to "received_values"
-        lastframe+=1 #move frame pointer
-    return lastframe #return frame pointer
+def combineAndAnalyze(rawFrames):
+    #rawFrames is an array of 2 frames
+    frameOne= numpy.array(rawFrames[0]) #get first raw frame
+    frameTwo = numpy.array(rawFrames[1]) #get second raw frame
+    combinedFrame= numpy.concatenate((frameOne,frameTwo),axis=1) #combine frames
+    # My understanding I am combining two numpy objects as one array, not a python array, so Analyze is being called
+    # on 1 numpy object with an array of length 20 since each small array is length 10
+    Analyze(combinedFrame) #call analyze on combined frames, Analyze should append combined numpy array to "received_values"
+    checkAnalyze.append(combinedFrame)
+    rawFrames.pop(0)
+    return rawFrames#return array with n - 1 arrays
 
 def Receiver(): #Read is sending a "stream" so a real world implementation would need a "receiver" for the data
                 #Hence the creation of this function to emulate a Receiver that gets the stream data and stores it
@@ -23,6 +25,7 @@ def Receiver(): #Read is sending a "stream" so a real world implementation would
     return frameStreamData
 
 def main():
+    global frameStreamData
     # Write Code Here plus any necessary helper functions
     #at the end of .5 seconds, look at how many elements are in the array of generated values
     #take note of the very last index,
@@ -32,22 +35,27 @@ def main():
     #Analyze((3,4))
     #received values array should be [(1,2),(2,3),(3,4)]
     #                                  obj   obj   obj
-    firstCall = time.time()
-    trackFrame = 0 #frame pointer for combineAndAnalyze
-    while(True):
-        readData = Receiver() #Get the Data from Read
-        if (readData == None):
-            print("Null value returned, terminating")
-            exit(-1)
-        curTime = time.time() #Take note of the current time
-        if((curTime-firstCall) >= 0.5): #check if .5 seconds has passed since last analysis
+    nullVal = False
+    timeStart = time.time()
+    readCalls = 0#frame pointer for combineAndAnalyze
+    firstRead = Receiver()
+    readCalls+=1
+    if(firstRead is not None):
+        while(not nullVal):
+            curTime = time.time() #Take note of the current time
+            if((curTime-timeStart) >= 0.5): #check if .5 seconds has passed since last analysis
             #This check is slightly off because of the time it takes to run the code. There are things that could
             #be done to improve precision, but I am operating under the assumption that 0.5 seconds is any value
             # between 0.5 and 0.599... seconds
-            trackFrame = combineAndAnalyze(readData,trackFrame) #Combine data in Analyze function and append it to Analyze
-            #"storage". Then return the index of frameStreamData that was read last.
-            #combine and analzye does the work
-            firstCall = curTime
+                readData = Receiver()
+                readCalls += 1  #Get the Data from Read iff .5 seconds have elapsed
+                if(readData is None):
+                    nullVal = True
+                if(readCalls>=2):
+                    frameStreamData = combineAndAnalyze(readData) #Combine data in Analyze function and append it to Analyze
+                 #"storage". Then return the index of frameStreamData that was read last.
+                # #combine and analzye does the work
+                timeStart = curTime
     pass
 
 if __name__ == "__main__":
