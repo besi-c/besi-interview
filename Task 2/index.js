4317826c766796b8e27b3ed6f9c7c7e2c78b550a
Fiji Marcelin <fiji4real@gmail.com>
2022-04-20 01:29:00 -0400

    fixed Task2, reviewing systemd

--- Task 2/index.js
+++ Task 2/index.js
@@ -4,16 +4,23 @@ const cors = require("cors")
 const app = express()
 const apiRoot = '/api'
 app.use(cors({origin:'*'}));
-var getServerTime= new Date().getTime();
-var serverDate = new Date(getServerTime)
-serverDate = serverDate.toString()
+function makeDate(){
+    var getServerTime= new Date().getTime();
+    var serverDate = new Date(getServerTime)
+    serverDate = serverDate.toUTCString() //wasn't returning proper string initially due to wrong string method. Was
+    //returning GMT-400 instead of GMT due to improper use of toString instead of toUTCString.
+    return serverDate;
+}
+
+
+
 
 app.get("/",(req,res)=>{
     res.send("Hello World")
 })
 
-app.get("/current-server-time",(req,res)=>{
-    res.send(serverDate)
+app.get("/api/current-server-time",(req,res)=>{
+    res.send(makeDate())
 })
 
 app.listen(8080)
